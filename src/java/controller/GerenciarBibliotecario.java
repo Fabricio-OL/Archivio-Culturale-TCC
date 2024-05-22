0a7f45523a6b4b85fd2577c5c3eebe6f8e5c1a9a
Fabricio Oliveira Lima <fabriccio.o.lima@gmail.com>
2024-05-21 22:03:53 -0300

    Tentativa de implementação CRUD bibliotecario

--- src/java/controller/GerenciarBibliotecario.java
+++ src/java/controller/GerenciarBibliotecario.java
@@ -7,14 +7,18 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.pessoa.Bibliotecario;
+import dao.BibliotecarioDAO;
+import java.sql.Date;
 
 /**
  *
- * @author Claudia
+ * @author jxavi
  */
 public class GerenciarBibliotecario extends HttpServlet {
 
@@ -30,18 +34,7 @@ public class GerenciarBibliotecario extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet GerenciarBibliotecario</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet GerenciarBibliotecario at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -56,7 +49,53 @@ public class GerenciarBibliotecario extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
+        PrintWriter out = response.getWriter();
+        String mensagem = "";
+
+        String acao = request.getParameter("acao");
+        String id = request.getParameter("idBibliotecario");
+
+        Bibliotecario bibliotecario = new Bibliotecario();
+
+        try {
+            BibliotecarioDAO bibliotecarioDAO = new BibliotecarioDAO();
+            
+            if (acao.equals("alterar")) {
+                
+                bibliotecario = bibliotecarioDAO.getCarregaPorID(Integer.parseInt(id));
+                
+                if (bibliotecario.getIdBibliotecario() > 0) {
+                    
+                    RequestDispatcher disp = getServletContext().getRequestDispatcher("/form_bibliotecario.jsp");
+                    request.setAttribute("bibliotecario", bibliotecario);
+                    disp.forward(request, response);
+                    
+                } else {
+                    
+                    mensagem = "Bibliotecario não encontrado";
+                    
+                }
+            }
+            if(acao.equals("delete")){
+                
+                bibliotecario.setIdBibliotecario(Integer.parseInt(id));
+                
+                if(bibliotecarioDAO.deletar(bibliotecario)){
+                    mensagem = "Deletado com sucesso";
+                }else{
+                    mensagem = "Erro ao excluir bibliotecario";
+                }
+            }
+        } catch (Exception e) {
+            mensagem = e.toString();
+        }
+
+        out.println("<script type='text/javascript'>");
+        out.println("alert('" + mensagem + "');");
+        out.println("location.href='estoque.jsp';");
+        out.println("</script>");
+
     }
 
     /**
@@ -70,7 +109,60 @@ public class GerenciarBibliotecario extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        PrintWriter out = response.getWriter();
+        String id = request.getParameter("idBibliotecario");
+        String nome = request.getParameter("nome");
+        String cpf = request.getParameter("cpf");
+        String dn = request.getParameter("dn");
+        String end = request.getParameter("end");
+        String salario = request.getParameter("salario");
+
+
+        String mensagem = "";
+
+        Bibliotecario bibliotecario = new Bibliotecario();
+        
+        try {
+            BibliotecarioDAO bibliotecarioDAO = new BibliotecarioDAO();
+
+            if (!id.isEmpty()) {
+                bibliotecario.setIdBibliotecario(Integer.parseInt(id));
+                
+            }else{
+                bibliotecario.setIdBibliotecario(0);
+                
+            }
+
+            if (nome.equals("") || nome.isEmpty()) {
+                mensagem = "Campos obrigatorios deverão ser preenchidos";
+                
+            } else {
+                bibliotecario.setNome(nome);
+                bibliotecario.setCpf(cpf);
+                bibliotecario.setDn(Date.valueOf(dn));
+                bibliotecario.setEnd(end);
+                bibliotecario.setSalario(Double.parseDouble(salario));
+
+                if (bibliotecarioDAO.gravar(bibliotecario)) {
+                    mensagem = "Gravado com sucesso";
+                    
+                } else {
+                    mensagem = "Erro ao gravar no banco de dados";
+                    
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+            //out.print(e);
+            mensagem = e.toString();
+        }
+
+        out.println("<script type='text/javascript'>");
+        out.println("alert('" + mensagem + "');");
+        out.println("location.href='estoque.jsp';");
+        out.println("</script>");
     }
 
     /**
